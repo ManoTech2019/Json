bbbf949233c6af60a8e48ad1fb4697215b4a5a2b
Emanuel Vitor <47760423+ManoTech2019@users.noreply.github.com>
2020-06-25 16:25:05 -0300

    Add files via upload

---
+++ index (cópia).js
@@ -0,0 +1,25 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente(
+    {
+
+    }
+);
+client1Nome = "Emanuel";
+client1CPF = 09442730681;
+client1Agencia = 1004;
+client1Saldo = 0;
+
+ client2Nome = "Leticia";
+ client2CPF = 0889833382;
+ client2Agencia = 1004;
+ client2Saldo = 0;
+
+
+
+console.log(client1Nome);
